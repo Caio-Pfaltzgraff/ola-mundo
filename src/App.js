37d19247d895882b2443453a34c7bd2d2f7db9ca
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-10-31 11:33:52 -0300

    Links do React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/aboutme";
 import Start from "./pages/start";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
+
       <Routes>
         <Route path="/" element={<Start />} />
         <Route path="/sobremim" element={<AboutMe />} />
