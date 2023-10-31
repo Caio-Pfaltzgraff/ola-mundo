b9a54aa75c74debce9e11da887a18e8a6214e5da
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-10-31 18:40:48 -0300

    Instalando o react-markdown

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,10 +1,28 @@
-import { useParams } from "react-router-dom"
+import './Post.css'
+
+import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModel from "components/PostModel";
+import Markdown from "react-markdown";
 
 export default function Post() {
     const params = useParams();
-    console.log(params)
+    
+    const post = posts.find((post) => {
+        return post.id === Number(params.id);
+    })
 
     return (
-        <h1>Post {params.id}</h1>
+        <PostModel
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            {/* Utilizar css normal pois trata-se de um markdown */}
+            <div className="post-markdown-container"> 
+                <Markdown>
+                    {post.texto}
+                </Markdown>
+            </div>
+        </PostModel>
     )
 }
