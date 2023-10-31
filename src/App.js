2ab04fb007eb26183ef02726cf6ed7c12c52ecb9
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2023-10-31 10:55:22 -0300

    Criando o roteador e algumas rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/aboutme";
 import Start from "./pages/start";
 
 function App() {
   return (
     <BrowserRouter>
-      
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/sobremim" element={<AboutMe />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
     </BrowserRouter>
   );
 }
